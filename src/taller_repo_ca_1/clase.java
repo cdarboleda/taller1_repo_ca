411eb1ff1c13e56943459a437725e6a5899a64d3
Neomett <Neomett@DESKTOP-JL93FHQ>
2022-05-17 20:52:46 -0500

    Mi segundo comentario para GitHub

--- src/taller_repo_ca_1/clase.java
+++ src/taller_repo_ca_1/clase.java
@@ -3,6 +3,7 @@ package taller_repo_ca_1;
 public class clase {
 	public static void main(String[] args) {
 		System.out.println("Mi primer proyecto JAVA en GitHub");
+		System.out.println("Otra impresión para mi proyecto en GitHub desde STS");
 	}
 
 }
